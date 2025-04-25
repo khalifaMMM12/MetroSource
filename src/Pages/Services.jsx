a7af000876bba77765484d434f406417cbdffb6f
Clemon Ezeh <clemonezeh@gmail.com>
2025-04-25 16:11:33 +0100

    added sub services

--- src/Pages/Services.jsx
+++ src/Pages/Services.jsx
@@ -1,4 +1,14 @@
-import { Box, Button, Flex, Image, Text } from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  CloseButton,
+  Dialog,
+  Flex,
+  Image,
+  List,
+  Portal,
+  Text,
+} from "@chakra-ui/react";
 
 import { FaHelmetSafety, FaArrowRight } from "react-icons/fa6";
 
@@ -14,14 +24,31 @@ const Services = () => {
       icon: <FaPencilRuler size={24} color='#121212' />,
       img: service3,
       title: "Architectural and engineering consultancy services",
+      subService: [
+        "PROJECT PLANNING AND DESIGN",
+        "URBAN DESIGN AND LANDSCAPE",
+        "INTERIOR DESIGN",
+        "BUILDING TRANSFORMATION AND ADAPTIVE REUSE",
+        "PROJECT MANAGEM",
+      ],
       subTitle:
         "We specialize in providing innovative and high-quality architectural designs for your construction projects. Our team of experienced architects and designers are dedicated to delivering exceptional services that meet and exceed our clients expectations.",
     },
+
     {
       id: 2,
       icon: <FaHelmetSafety size={24} color='#121212' />,
       img: service2,
       title: "Engineering Services",
+      subService: [
+        "BUILDING CONCONSTRUCTION AND ENGINEERING",
+        "STRUCTURAL ENGINEERING",
+        "MEP ENGINEERING",
+        "CIVIL ENGINEERING",
+        "PROJECT MANAGEMENT",
+        "PROJECT MONITORING AND EVALUATION",
+        "ENVIRONMENTAL IMPACT ANALYSIS",
+      ],
       subTitle:
         "Our skilled engineers provide diverse services, prioritizing quality construction with specialized solutions. Using renowned ‘Green technology,’ we ensure safe, efficient, and cost-effective outcomes.",
     },
@@ -101,18 +128,61 @@ const Services = () => {
               <Flex position={"relative"}>
                 <Image src={item.img} alt={item.title} />
                 <Flex position={"absolute"} bottom={4} gap={1} right={4}>
-                  <Button
-                    bg={"#ef7826"}
-                    className='hover-BTN'
-                    color={"#ffffff"}
-                    px={2}
-                    rounded={"full"}>
-                    Learn More
-                  </Button>
+                  <Dialog.Root
+                    size='lg'
+                    placement='center'
+                    motionPreset='slide-in-bottom'>
+                    <Dialog.Trigger>
+                      <Button
+                        bg={"#ef7826"}
+                        className='hover-BTN'
+                        color={"#ffffff"}
+                        px={2}
+                        rounded={"full"}>
+                        Learn More
+                      </Button>
 
-                  <Button bg={"#ffffff"} className='hover-BTN' rounded={"full"}>
-                    <FaArrowRight color={"#ef7826"} size={13} />
-                  </Button>
+                      <Button
+                        bg={"#ffffff"}
+                        className='hover-BTN'
+                        rounded={"full"}>
+                        <FaArrowRight color={"#ef7826"} size={13} />
+                      </Button>
+                    </Dialog.Trigger>
+                    <Portal>
+                      <Dialog.Backdrop />
+                      <Dialog.Positioner>
+                        <Dialog.Content bg={"#121212"}>
+                          <Dialog.Header p={4} position={"relative"}>
+                            <Dialog.Title color={"#ffffff"}>
+                              {item.title} Sub-services
+                            </Dialog.Title>
+                            <Dialog.CloseTrigger
+                              pos={"absolute"}
+                              right={5}
+                              rounded={"full"}
+                              top={5}
+                              bg={"#ffffff"}
+                              asChild>
+                              <CloseButton size='sm' color={"#121212"} />
+                            </Dialog.CloseTrigger>
+                          </Dialog.Header>
+                          <Dialog.Body padding={4}>
+                            <List.Root gap={4} listStyle={"none"}>
+                              {item.subService.flatMap((list) => (
+                                <List.Item
+                                  color={"#ffffff"}
+                                  fontSize={16}
+                                  fontWeight={500}>
+                                  {list}
+                                </List.Item>
+                              ))}
+                            </List.Root>
+                          </Dialog.Body>
+                        </Dialog.Content>
+                      </Dialog.Positioner>
+                    </Portal>
+                  </Dialog.Root>
                 </Flex>
               </Flex>
               <Flex direction='column' gap={2} p={2}>
